3d5251f082531db8f8bbce70e126ac1c914e2259
github-action-benchmark <github@users.noreply.github.com>
2021-10-04 13:17:49 +0000

    add Go Benchmark (go) benchmark result for 3f95a5d5bfb998338cbc5a10b96c1a8dccd616c1

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1633351487445,
+  "lastUpdate": 1633353469638,
   "repoUrl": "https://github.com/isdevx/cerbos",
   "entries": {
     "Go Benchmark": [
@@ -26,6 +26,56 @@ window.BENCHMARK_DATA = {
             "value": 5903836,
             "unit": "ns/op\t 1478074 B/op\t   34020 allocs/op",
             "extra": "201 times\n2 procs"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "name": "isdevx",
+            "username": "isdevx"
+          },
+          "committer": {
+            "name": "isdevx",
+            "username": "isdevx"
+          },
+          "id": "3f95a5d5bfb998338cbc5a10b96c1a8dccd616c1",
+          "message": "Create benchmark.yml",
+          "timestamp": "2021-08-06T08:37:33Z",
+          "url": "https://github.com/isdevx/cerbos/pull/7/commits/3f95a5d5bfb998338cbc5a10b96c1a8dccd616c1"
+        },
+        "date": 1633353467687,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkCompile",
+            "value": 6466442,
+            "unit": "ns/op\t 1512970 B/op\t   35084 allocs/op",
+            "extra": "199 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkIntersect50",
+            "value": 15108,
+            "unit": "ns/op",
+            "extra": "80590 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkIntersect25",
+            "value": 8407,
+            "unit": "ns/op",
+            "extra": "132048 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkIntersect15",
+            "value": 5862,
+            "unit": "ns/op",
+            "extra": "203470 times\n2 procs"
+          },
+          {
+            "name": "BenchmarkIntersect5",
+            "value": 3322,
+            "unit": "ns/op",
+            "extra": "360535 times\n2 procs"
           }
         ]
       }
