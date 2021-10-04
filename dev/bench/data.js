0e926feec77d1c2c90d6179271293009813fa32f
github-action-benchmark <github@users.noreply.github.com>
2021-10-04 12:44:47 +0000

    add Go Benchmark (go) benchmark result for bacb571c86813507a0134d42080b9c0a5932dc64

---
+++ dev/bench/data.js
@@ -0,0 +1,34 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1633351487445,
+  "repoUrl": "https://github.com/isdevx/cerbos",
+  "entries": {
+    "Go Benchmark": [
+      {
+        "commit": {
+          "author": {
+            "name": "isdevx",
+            "username": "isdevx"
+          },
+          "committer": {
+            "name": "isdevx",
+            "username": "isdevx"
+          },
+          "id": "bacb571c86813507a0134d42080b9c0a5932dc64",
+          "message": "Create benchmark.yml",
+          "timestamp": "2021-08-06T08:37:33Z",
+          "url": "https://github.com/isdevx/cerbos/pull/7/commits/bacb571c86813507a0134d42080b9c0a5932dc64"
+        },
+        "date": 1633351484934,
+        "tool": "go",
+        "benches": [
+          {
+            "name": "BenchmarkCompile",
+            "value": 5903836,
+            "unit": "ns/op\t 1478074 B/op\t   34020 allocs/op",
+            "extra": "201 times\n2 procs"
+          }
+        ]
+      }
+    ]
+  }
+}
